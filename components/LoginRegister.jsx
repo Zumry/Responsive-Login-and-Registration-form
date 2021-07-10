fd6c7701790fa3e38c7ee1d62dbe16c9df0fab01
Zumry <amzumry@gmail.com>
2021-07-10 23:01:25 +0530

    initial commit

--- components/LoginRegister.jsx
+++ components/LoginRegister.jsx
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
+import '../styles/LoginRegister.css';
+
+//Images
+import Img011 from 'url:../images/LoginRegister/bg1.svg';
+import Img022 from 'url:../images/LoginRegister/bg2.svg';
+import ImgEmail from 'url:../images/LoginRegister/email.png';
+import ImgLock from 'url:../images/LoginRegister/lock.png';
+import ImgUser from 'url:../images/LoginRegister/user.png';
+import ImgUsers from 'url:../images/LoginRegister/users.png';
+import ImgEye from 'url:../images/LoginRegister/eye.png';
+import ImgEyeHide from 'url:../images/LoginRegister/eye-hide.png';
 
 /**
  * @author : Zumry Amanullah
+ * @portfolio : https://www.zumry.ml
  */
 
 class LoginRegister extends Component {
