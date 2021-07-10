6d1fc785df1848bf7769acf91ae034bb69abd84a
Zumry <amzumry@gmail.com>
2021-07-10 22:13:36 +0530

    initial commit

---
+++ components/LoginRegister.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+/**
+ * @author : Zumry Amanullah
+ */
+
+class LoginRegister extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default LoginRegister;
