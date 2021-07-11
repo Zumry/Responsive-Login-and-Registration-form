592e4e4af72e79851df056af687c04f9f8bc7f58
Zumry <amzumry@gmail.com>
2021-07-11 21:37:23 +0530

    Login form implemented

--- components/LoginRegister.jsx
+++ components/LoginRegister.jsx
@@ -19,14 +19,86 @@ import ImgEyeHide from 'url:../images/LoginRegister/eye-hide.png';
 class LoginRegister extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            isActive:true
+        }
+    }
+
+    onChange(event){
+        const { name, value } = event.target;
+        this.setState({ [name] : value });
     }
 
     render() {
-        return (
-            <div>
-                <h2>Hello</h2>
+        return <div className={"Login-Section"}>
+            <div className={this.state.isActive===true ? "container" : "container sign-up-mode"} id={"container"}>
+                <div className={"forms-container"}>
+                    <div className={"signin-signup"}>
+                        <form className={"sign-in-form"}>
+                            <h2 className={"title"}>Sign in</h2>
+                            <div className={"input-field"}>
+                                <i className={"fas fa-user"}></i>
+                                <input type="text" placeholder={"username"} />
+                            </div>
+                            <div className={"input-field"}>
+                                <i className={"fas fa-lock"}></i>
+                                <input type="password" placeholder={"Password"} />
+                            </div>
+                            <input type="submit" value={"Login"} className={"btn solid"} />
+                            <p className={"social-text"}> or Sign in with social platforms </p>
+                            <div className={"social-media"}>
+                                <a href={"#"} className={"social-icon"}>
+                                    <i className={"fab fa-facebook-f"}></i>
+                                </a>
+                                <a href={"#"} className={"social-icon"}>
+                                    <i className={"fab fa-twitter"}></i>
+                                </a>
+                                <a href={"#"} className={"social-icon"}>
+                                    <i className={"fab fa-google"}></i>
+                                </a>
+                                <a href={"#"} className={"social-icon"}>
+                                    <i className={"fab fa-linkedin-in"}></i>
+                                </a>
+                            </div>
+                        </form>
+
+                        <form className={"sign-up-form"}>
+                            <h2 className={"title"}>Sign up</h2>
+                            <div className={"input-field"}>
+                                <i className={"fas fa-user"}></i>
+                                <input type={"text"} placeholder={"Username"} />
+                            </div>
+                            <div className={"input-field"}>
+                                <i className={"fas fa-envelop"}></i>
+                                <input type={"email"} placeholder={"Email"} />
+                            </div>
+                            <div className={"input-field"}>
+                                <i className={"fas fa-lock"}></i>
+                                <input type={"password"} placeholder={"password"} />
+                            </div>
+                            <input type={"submit"} className={"btn"} value={"sign up"} />
+                            <p className={"social-media"}>Or Sign up with social platforms</p>
+                            <div className={"social-media"}>
+                                <a href={"#"} className={"social-icon"}>
+                                    <i className={"fab fa-facebook-f"}></i>
+                                </a>
+                                <a href={"#"} className={"social-icon"}>
+                                    <i className={"fab fa-twitter"}></i>
+                                </a>
+                                <a href={"#"} className={"social-icon"}>
+                                    <i className={"fab fa-google"}></i>
+                                </a>
+                                <a href={"#"} className={"social-icon"}>
+                                    <i className={"fab fa-linkedin-in"}></i>
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
             </div>
-        );
+        </div>
     }
 }
 
